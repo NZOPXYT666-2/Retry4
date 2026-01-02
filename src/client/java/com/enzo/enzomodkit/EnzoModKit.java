50d2248998ea8dc1cd587dfddcb806b2fc04517a
NZOPXYT666-2 <ashazpro666@gmail.com>
2026-01-02 17:28:18 +0530

    Create EnzoModKit.java

---
+++ src/client/java/com/enzo/enzomodkit/EnzoModKit.java
@@ -0,0 +1,10 @@
+package com.enzo.enzomodkit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnzoModKit implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("EnzoModKit loaded!");
+    }
+}
